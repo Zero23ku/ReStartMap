930b39ba0fe1b68d8005eae05b41b5757ad8b059
zero23ku <marcelo.cardenas@usach.cl>
2017-07-21 08:19:22 -0400

    Added style.css and index.php, also modified files ad folder permission

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Ragnarok Re:Start Mob Spawns</title>
+		<link rel="stylesheet" type="text/css" href="files/css/style.css">
+	</head>
+	<body>
+		<p>TEST</p>
+	</body>
+</html>
