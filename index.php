459626e77aa07ee68a87c780fd5892e765722089
zero23ku <marcelo.cardenas@usach.cl>
2017-07-24 11:59:38 -0400

    around 50% map done

--- index.php
+++ index.php
@@ -132,148 +132,425 @@
 			</div>
 			<div class="clear"></div>
 			<div class="row-map">
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-			</div>
-			<div class="clear"></div>
-			<div class="row-map">
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-			</div>
-			<div class="clear"></div>
-			<div class="row-map">
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-			</div>
-			<div class="clear"></div>
-			<div class="row-map">
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
+				
+				<div class="map" id="gef_fild08">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild08.png"/>
+				</div>
+				
+				<div class="map" id="gef_fild07">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild07.png"/>
+				</div>
+				
+				<div class="map" id="geffen">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/geffen.png"/>
+				</div>
+				
+				<div class="map" id="gef_fild00">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild00.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild00">
+					<img class="map" id="img_size" src="files/img/world/prt_fild00.png"/>
+				</div>
+				
+				<div class="map" id="mjolnir_09">
+					<img class="map" id="img_size" src="files/img/world/aldebaranfields/mjolnir_09.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild01">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild01.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild02">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild02.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild03">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild03.png"/>
+				</div>
+				
+				<div class="map" id="prt_monk">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_monk.png"/>
+				</div>
+				
 				<div class="map"></div>
 			</div>
 			<div class="clear"></div>
 			<div class="row-map">
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
+				<div class="map" id="gef_fild12">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild12.png"/>
+				</div>
+				
+				<div class="map" id="gef_fild13">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild13.png"/>
+				</div>
+				
+				<div class="map" id="gef_fild09">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild09.png"/>
+				</div>
+				
+				<div class="map" id="gef_fild01">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild01.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild04">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild04.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild05">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild05.png"/>
+				</div>
+				
+				<div class="map" id="prontera">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prontera.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild06">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild06.png"/>
+				</div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
+				
 				<div class="map"></div>
 			</div>
 			<div class="clear"></div>
 			<div class="row-map">
+				
 				<div class="map"></div>
+				
+				<div class="map" id="umbala">
+					<img class="map" id="img_size" src="files/img/world/umbalafields/umbala.png"/>
+				</div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
+				<div class="map" id="gef_fild14">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild14.png"/>
+				</div>
+				
+				<div class="map" id="gef_fild10">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild10.png"/>
+				</div>
+				
+				<div class="map" id="gef_fild03">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild03.png"/>
+				</div>
+				
+				<div class="map" id="gef_fild02">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild02.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild07">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild07.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild08">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild08.png"/>
+				</div>
+				
+				<div class="map" id="izlude">
+					<img class="map" id="img_size" src="files/img/world/izlude/izlude.png"/>
+				</div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
+
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
 				<div class="map"></div>
+				
+				<div class="map" id="um_fild04">
+					<img class="map" id="img_size" src="files/img/world/umbalafields/um_fild04.png"/>
+				</div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
+				<div class="map" id="gef_fild11">
+					<img class="map" id="img_size" src="files/img/world/geffenfields/gef_fild11.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild11">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild11.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild10">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild11.png"/>
+				</div>
+				
+				<div class="map" id="prt_fild09">
+					<img class="map" id="img_size" src="files/img/world/pronterafields/prt_fild09.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild01">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild01.png"/>
+				</div>
+				
+				<div class="map" id="pay_fild04">
+					<img class="map" id="img_size" src="files/img/world/payonfields/pay_fild04.png"/>
+				</div>
+
+				<div class="map" id="pay_arche">
+					<img class="map" id="img_size" src="files/img/world/payonfields/pay_arche.png"/>
+				</div>
+				
 				<div class="map"></div>
+				
+				
 				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				
+				<div class="map" id="um_fild01">
+					<img class="map" id="img_size" src="files/img/world/umbalafields/um_fild01.png"/>
+				</div>
+				
+				<div class="map" id="um_fild02">
+					<img class="map" id="img_size" src="files/img/world/umbalafields/um_fild02.png"/>
+				</div>
+				
+				<div class="map" id="um_fild03">
+					<img class="map" id="img_size" src="files/img/world/umbalafields/um_fild03.png"/>
+				</div>
+				
 				<div class="map"></div>
+				
+				<div class="map"></div>
+				
+				<div class="map" id="moc_ruins">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_ruins.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild07">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild07.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild06">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild06.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild05">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild05.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild04">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild04.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild02">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild02.png"/>
+				</div>
+				
+				<div class="map" id="pay_gld">
+					<img class="map" id="img_size" src="files/img/world/payonfields/pay_gld.png"/>
+				</div>
+				
+				<div class="map" id="payon">
+					<img class="map" id="img_size" src="files/img/world/payonfields/payon.png"/>
+				</div>
+				
+				<div class="map" id="pay_fild08">
+					<img class="map" id="img_size" src="files/img/world/payonfields/pay_fild08.png"/>
+				</div>
+				
+				<div class="map" id="pay_fild09">
+					<img class="map" id="img_size" src="files/img/world/payonfields/pay_fild09.png"/>
+				</div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+
+				<div class="map"></div>
+
+				<div class="map"></div>
+				
+				<div class="map" id="cmd_fild01">
+					<img class="map" id="img_size" src="files/img/world/comodofields/cmd_fild01.png"/>
+				</div>
+				
+				<div class="map" id="cmd_fild03">
+					<img class="map" id="img_size" src="files/img/world/comodofields/cmd_fild03.png"/>
+				</div>
+				
+				<div class="map" id="cmd_fild05">
+					<img class="map" id="img_size" src="files/img/world/comodofields/cmd_fild05.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild19">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild19.png"/>
+				</div>
+				
+				<div class="map" id="morroc">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/morocc.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild10">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild10.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild09">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild09.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild08">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild08.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild13">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild13.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild03">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild03.png"/>
+				</div>
+				
+				<div class="map" id="pay_fild01">
+					<img class="map" id="img_size" src="files/img/world/payonfields/pay_fild01.png"/>
+				</div>
+				
+				<div class="map" id="pay_fild07">
+					<img class="map" id="img_size" src="files/img/world/payonfields/pay_fild07.png"/>
+				</div>
+				
+				<div class="map" id="pay_fild10">
+					<img class="map" id="img_size" src="files/img/world/payonfields/pay_fild10.png"/>
+				</div>
 			</div>
 			<div class="clear"></div>
 			<div class="row-map">
 				<div class="map" id="comodo">
-					<img id="comodo_img" src="files/img/world/comodofields/comodo.png"/>
+					<img class="img_size" class="map" src="files/img/world/comodofields/comodo.png"/>
 				</div>
+
 				<div class="map"></div>
-				<div class="map" id="cmd_fild02"></div>
-				<div class="map" id="cmd_fild04"></div>
-				<div class="map" id="cmd_fild06"></div>
-				<div class="map" id="cmd_fild08"></div>
-				<div class="map" id="moc_fild12"></div>
-				<div class="map" id="moc_fild11"></div>
-				<div class="map" id="moc_fild15"></div>
-				<div class="map" id="moc_fild14"></div>
+
+				<div class="map" id="cmd_fild02">
+					<img class="img_size" class="map" src="files/img/world/comodofields/cmd_fild02.png"/>
+				</div>
+
+				<div class="map" id="cmd_fild04">
+					<img class="img_size" class="map" src="files/img/world/comodofields/cmd_fild04.png"/>
+				</div>
+
+				<div class="map" id="cmd_fild06">
+					<img class="img_size" class="map" src="files/img/world/comodofields/cmd_fild06.png"/>
+				</div>
+
+				<div class="map" id="cmd_fild08">
+					<img class="img_size" class="map" src="files/img/world/comodofields/cmd_fild08.png"/>
+				</div>
+
+				<div class="map" id="moc_fild12">
+					<img class="img_size" class="map" src="files/img/world/morrocfields/moc_fild12.png"/>
+				</div>
+
+				<div class="map" id="moc_fild11">
+					<img class="img_size" class="map" src="files/img/world/morrocfields/moc_fild11.png"/>
+				</div>
+
+				<div class="map" id="moc_fild15">
+					<img class="img_size" class="map" src="files/img/world/morrocfields/moc_fild15.png"/>
+				</div>
+
+				<div class="map" id="moc_fild14">
+					<img class="img_size" class="map" src="files/img/world/morrocfields/moc_fild14.png"/>
+				</div>
+
 				<div class="map"></div>
-				<div class="map" id="pay_fild11"></div>
-				<div class="map" id="pay_fiñd02"></div>
-				<div class="map" id="pay_fild03"></div>
-				<div class="map" id="alberta"></div>
+				<div class="map" id="pay_fild11">
+					<img class="img_size" class="map" src="files/img/world/payonfields/pay_fild11.png"/>
+				</div>
+
+				<div class="map" id="pay_fild02">
+					<img class="img_size" class="map" src="files/img/world/payonfields/pay_fild02.png"/>
+				</div>
+
+				<div class="map" id="pay_fild03">
+					<img class="img_size" class="map" src="files/img/world/payonfields/pay_fild03.png"/>
+				</div>
+
+				<div class="map" id="alberta">
+					<img class="img_size" class="map" src="files/img/world/alberta/alberta.png"/>
+				</div>
 			</div>
 			<div class="clear"></div>
 			<div class="row-map">
 				<div class="map"></div>
+
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
+				<div class="map" id="cmd_fild07">
+					<img class="map" id="img_size" src="files/img/world/comodofields/cmd_fild07.png"/>
+				</div>
+				
+				<div class="map" id="cmd_fild09">
+					<img class="map" id="img_size" src="files/img/world/comodofields/cmd_fild09.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild18">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild18.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild17">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild17.png"/>
+				</div>
+				
+				<div class="map" id="moc_fild16">
+					<img class="map" id="img_size" src="files/img/world/morrocfields/moc_fild16.png"/>
+				</div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
+				
 				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
-				<div class="map"></div>
+				
+				<div class="map" id="pay_fild05">
+					<img class="map" id="img_size" src="files/img/world/payonfields/pay_fild05.png"/>	
+				</div>
+				
+				<div class="map" id="pay_fild06">
+					<img class="map" id="img_size" src="files/img/world/payonfields/pay_fild06.png"/>
+				</div>
+				
 				<div class="map"></div>
 			</div>
 			<div class="clear"></div>
