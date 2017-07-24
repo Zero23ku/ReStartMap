216355e5096306404a24225cd6829e1c1fe14170
zero23ku <marcelo.cardenas@usach.cl>
2017-07-24 10:04:13 -0400

    layout to display map created, added images of world

--- index.php
+++ index.php
@@ -4,6 +4,282 @@
 		<link rel="stylesheet" type="text/css" href="files/css/style.css">
 	</head>
 	<body>
-		<p>TEST</p>
+		<div id="map-container">
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map" id="comodo">
+					<img id="comodo_img" src="files/img/world/comodofields/comodo.png"/>
+				</div>
+				<div class="map"></div>
+				<div class="map" id="cmd_fild02"></div>
+				<div class="map" id="cmd_fild04"></div>
+				<div class="map" id="cmd_fild06"></div>
+				<div class="map" id="cmd_fild08"></div>
+				<div class="map" id="moc_fild12"></div>
+				<div class="map" id="moc_fild11"></div>
+				<div class="map" id="moc_fild15"></div>
+				<div class="map" id="moc_fild14"></div>
+				<div class="map"></div>
+				<div class="map" id="pay_fild11"></div>
+				<div class="map" id="pay_fiñd02"></div>
+				<div class="map" id="pay_fild03"></div>
+				<div class="map" id="alberta"></div>
+			</div>
+			<div class="clear"></div>
+			<div class="row-map">
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+				<div class="map"></div>
+			</div>
+			<div class="clear"></div>
+		</div>
+
+
+
 	</body>
 </html>
