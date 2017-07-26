b31a91e71ecebc86202a3cef1a333b3d53269179
zero23ku <marcelo.cardenas@usach.cl>
2017-07-25 20:32:49 -0400

    need to transform data in jquery to json format

--- files/php/process.php
+++ files/php/process.php
@@ -3,31 +3,47 @@
 	include 'connection.php';
 	
 	if( $_POST ){
-		$mob_id_name = $_POST['mob_id_name'];
+		$mob_id_name = json_decode($_POST['mob_id_name']);
 	}
 
-	$sql = "select 	restartmap.map.map_name
+	$sql = "select 	restartmap.map.map_name, restartmap.map.map_id
 			from	restartmap.map, restartmap.map_monster, restartmap.monster
 			where 	restartmap.map.map_id = restartmap.map_monster.map_monster_map_id and
 	  				restartmap.monster.monster_id = restartmap.map_monster.map_monster_monster_id and
-      				restartmap.monster.monster_id = '$mob_id_name' 
-      				or restartmap.monster.monster_name = '$mob_id_name';"
+      				(restartmap.monster.monster_id = '$mob_id_name' 
+      				or restartmap.monster.monster_name like '$mob_id_name')";
     $result = $conn->query($sql);
+
+    $rows = array();
+
+    while($data = $result->fetch_assoc()){
+    	$rows[] = $data;
+    }
+
+    $json_result = json_encode($rows);
     
     $conn->close();
+	*/
+
+    /*
+    <div id="result-container">
+		<?php
+			if($result->num_rows > 0 ){
+				while($row = $result->fetch_assoc()){
+					print "map: " . $row["map_name"] . " map_id: " .$row["map_id"] . "<br>";
+				}
+			}else{
+				print "<p> nothing :( </p>";
+			}
+
+
+		?>
+	</div>
+	*/
+
+	print($json_result);
 ?>
 
 
-<div id="result-container">
-	<?php
-		if($result->num_rows > 0 ){
-			while($row = $result->fetch_assoc()){
-				print "map: " . $row["restartmap.map.map_name"] . "<br>";
-			}
-		}else{
-			print "<p> nothing :( </p>";
-		}
 
 
-	?>
-</div>
